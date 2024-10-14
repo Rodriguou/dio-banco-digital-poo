9aba8f875a8e034f80d2708356d0ed383ddd1045
Rodrigo Cândido Matos <rodriguou2k20@gmail.com>
2024-10-13 21:28:09 -0300

    feat: add validation and encapsulation

--- src/Conta.java
+++ src/Conta.java
@@ -1,25 +1,31 @@
 public abstract class Conta implements OperacoesConta {
     private static final int AGENCIA_PADRAO = 1;
-    private static int SEQUENCIAL = 1;
+    private static int sequencial = 1;
 
-    protected int agencia;
-    protected int numero;
-    protected double saldo;
-    protected Cliente cliente;
+    private final int agencia;
+    private final int numero;
+    private double saldo;
+    private final Cliente cliente;
 
     public Conta(Cliente cliente) {
         this.agencia = AGENCIA_PADRAO;
-        this.numero = SEQUENCIAL++;
+        this.numero = sequencial++;
         this.cliente = cliente;
     }
 
     @Override
     public void sacar(double valor) {
+        if (valor <= 0 || valor > saldo) {
+            throw new IllegalArgumentException("Valor inválido para saque");
+        }
         saldo -= valor;
     }
 
     @Override
     public void depositar(double valor) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException("Valor inválido para depósito");
+        }
         saldo += valor;
     }
 
